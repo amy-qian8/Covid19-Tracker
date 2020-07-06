9fef8d6875c2fd8399cc184a1a7392f09ae28205
Amy Qian <67285038+amy-qian8@users.noreply.github.com>
2020-07-05 23:17:48 -0700

    Add files via upload

---
+++ covid19-tracker/precache-manifest.e61a863410daa1c4998935cc3a9fb2b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8400c05d75e95d90458913e66b430caa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a576e37429cfc42edbd2",
+    "url": "/static/css/main.61e06066.chunk.css"
+  },
+  {
+    "revision": "e3e8889849d7b627da88",
+    "url": "/static/js/2.a7ce2ba5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.a7ce2ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a576e37429cfc42edbd2",
+    "url": "/static/js/main.8cde4052.chunk.js"
+  },
+  {
+    "revision": "81ed1e4f7707c590606d",
+    "url": "/static/js/runtime-main.3a0c7701.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
